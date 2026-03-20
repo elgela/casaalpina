5be47981cd1ed6d8c170cd95f445e3dbc9602847
Marcelo Gelato <gelatomarcelo@hotmail.com>
2026-03-20 10:30:28 -0300

    nuevo archiivo

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+// Esto debe ir al inicio del archivo, antes de cualquier salida
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
